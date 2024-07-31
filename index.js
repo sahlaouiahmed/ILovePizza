e9217217cdb5192d5fec4eefd99a138f338f0973
sahlaouiahmed <sahlaouiahmad@outlook.com>
2024-07-31 10:11:55 +0000

    html and css code for the review section + images

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-/* swiper js for home slides from swiperjs.com */
+/* swiperjs javascript code for home slides from swiperjs.com */
 var swiper = new Swiper(".container", {
     spaceBetween: 150,
     centeredSlides: true,
     autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
+        delay: 5000,
+        disableOnInteraction: false,
     },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
-    loop:true,
+    loop: true,
+});
+
+/* swiperjs javascript code for review slides */
+var swiper = new Swiper(".review-block", {
+    spaceBetween: 150,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+    loop: true,
 });
